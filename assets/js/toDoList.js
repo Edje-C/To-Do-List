a9501c2e3e0c9818534cf0563f5d21b0f34bdb41
Edje-C <elekson9@yahoo.com>
2017-07-15 22:31:57 -0400

    Simple To Do List Web App

---
+++ assets/js/toDoList.js
@@ -0,0 +1,28 @@
+
+///check off todos
+$("ul").on("click","li",function(){
+	$(this).toggleClass("completed");
+});
+
+///delete todos
+$("ul").on("click","span",function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+///make new todos
+$("input[type='text']").keypress(function(event){
+	if(event.which===13){
+		///grab todo from input box
+		var newToDo = ($(this).val());
+		///creat new li
+		$("ul").append("<li>" + newToDo + " <span class='delete'>X</span></li>");
+		$(this).val("");
+	}
+});
+
+$("#deleteAll").click(function(){
+	$("li").remove();
+});
